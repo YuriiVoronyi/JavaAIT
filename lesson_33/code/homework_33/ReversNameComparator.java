675dcaf332b54b5aedb051623434321f5c55edec
fogroy2000 <livetrade2000@gmail.com>
2023-09-02 19:57:09 +0200

    change ReversNameComparator

--- lesson_33/code/homework_33/ReversNameComparator.java
+++ lesson_33/code/homework_33/ReversNameComparator.java
@@ -5,18 +5,12 @@ import java.util.Comparator;
 public class ReversNameComparator implements Comparator<Animals> {
     public int compare(Animals a1, Animals a2){
 
-        String reversName1 = a1.getAnimalsName();
-        String reversName2 = a2.getAnimalsName();
-
-        StringBuilder builder1 = new StringBuilder(reversName1);
-        StringBuilder builder2 = new StringBuilder(reversName2);
+        StringBuilder builder1 = new StringBuilder(a1.getAnimalsName());
+        StringBuilder builder2 = new StringBuilder(a2.getAnimalsName());
 
         builder1.reverse();
         builder2.reverse();
 
-        String str1 = builder1.toString();
-        String str2 = builder2.toString();
-
-        return str1.compareTo(str2);
+        return builder1.toString().compareTo(builder2.toString());
     }
 }
